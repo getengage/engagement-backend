302fb98f0c0fb695a93f7d7d0a3e027dfb0f2e0e
nick <ad60@nicks-MacBook-Pro.local>
2016-07-08 11:36:26 -0400

    saving progress

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -1,17 +1,23 @@
 module Api::V1
   class ReportsController < ApiController
     def create
-      binding.pry
       data = {
-        values: data_params,
-        timestamp: Time.now.to_i
+        tags: tag_params,
+        values: value_params
       }
-
-      # InfluxDB::Rails.write_point(data)
+      InfluxDB::Rails.client.write_point("events", data.merge(timestamp_param))
     end
 
-    def data_params
-      params.require(:data)
+    def tag_params
+      params.require(:data).permit(:api_key, :session_id, :source_url)
+    end
+
+    def timestamp_param
+      params.require(:data).permit(:timestamp)
+    end
+
+    def value_params
+      params.require(:data).permit(:referrer, :x_pos, :y_pos, :is_visible)
     end
   end
 end
