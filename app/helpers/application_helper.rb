c8bfb1e40ed8b2a56e4ce835de97ee34c941f503
Nick Soto <nickesoto@gmail.com>
2016-09-20 23:23:03 -0400

    style stats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def strip_url(url)
+    url.sub!(/https\:\/\/www./, '') if url.include? "https://www."
+    url.sub!(/http\:\/\/www./, '')  if url.include? "http://www."
+    url.sub!(/www./, '')            if url.include? "www."
+    url
+  end
 end
