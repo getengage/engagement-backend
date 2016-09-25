47ce87bf9c4f1920bddee4335d1d169b1fb49513
Nick Soto <nickesoto@gmail.com>
2016-09-25 10:12:00 -0400

    upgrade react on rails to v6

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,7 @@
-  // DO NOT REQUIRE jQuery or jQuery-ujs in this file!
-// DO NOT REQUIRE TREE!
-
-// CRITICAL that vendor-bundle must be BEFORE bootstrap-sprockets and turbolinks
+// CRITICAL that webpack-bundle must be BEFORE bootstrap-sprockets and turbolinks
 // since it is exposing jQuery and jQuery-ujs
 
-//= require vendor-bundle
-//= require app-bundle
+//= require webpack-bundle
 //= require turbolinks
 //= require foundation
 //= require jquery.dataTables.min
