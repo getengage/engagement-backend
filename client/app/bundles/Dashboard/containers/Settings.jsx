06edc05db5544c7fc9e48b3cd2672f6f94abc96f
Nick Soto <nickesoto@gmail.com>
2016-09-25 17:16:00 -0400

    rm lodash

--- client/app/bundles/Dashboard/containers/Settings.jsx
+++ client/app/bundles/Dashboard/containers/Settings.jsx
@@ -1,5 +1,4 @@
 import React, { PropTypes } from 'react';
-import _ from 'lodash';
 import SettingsTable from '../components/SettingsTable';
 import SettingsForm from '../components/SettingsForm';
 
