606a7e5dc6e3408819dd76f8ddbb80a4177826da
Nick Soto <nickesoto@gmail.com>
2016-10-21 23:04:44 -0400

    UI & Influql Updates
    - Fix "Unique Visits by Day" Influxql time ordering
    - make nav animation less janky using motion-ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,33 +8,19 @@
 //= require Chart.bundle.min
 
 document.addEventListener("turbolinks:load", function() {
-  $(document).foundation();
+  var doc      = $(document),
+      table    = doc.find('table'),
+      dropDown = doc.find('.dropdown_profile');
 
-  $('table').each(function() {
+  doc.foundation();
+
+  table.each(function() {
     $(this).dataTable({
       "bFilter": false,
       "bLengthChange": false,
       "bInfo": false,
       "bPaginate": true
     });
-  });
-
-  $('.dropdown_profile').on('show.zf.dropdownmenu', function() {
-    var dropdown = $(this).find('.is-dropdown-submenu');
-
-    if (!dropdown.hasClass('js-dropdown-animated')) {
-      dropdown.css('display', 'inherit');
-      dropdown.fadeIn({queue: false, duration: 'fast'});
-      dropdown.animate({ "top": "+=30" }, 250 );
-      dropdown.addClass('js-dropdown-animated');
-    }
-  });
-
-  $('.dropdown_profile').on('hide.zf.dropdownmenu', function() {
-    var dropdown = $(this).find('.is-dropdown-submenu');
-    dropdown.fadeOut({queue: false, duration: 'fast'});
-    dropdown.animate({ "top": "-=30" }, 250 );
-    dropdown.removeClass('js-dropdown-animated');
   });
 
   $('.inline-editable').on('change', function(e) {
