4102c617bf3110129746a6655fc0bcf470a678f0
Nick Soto <nickesoto@gmail.com>
2016-10-22 23:03:11 -0400

    codeclimate fix

--- app/jobs/event/scores_job.rb
+++ app/jobs/event/scores_job.rb
@@ -1,7 +1,7 @@
 class Event::ScoresJob < ActiveJob::Base
   queue_as :default
 
-  def perform(*args)
+  def perform
     Sidekiq::Client.push("queue" => "go_queue",
                          "class" => "RegressionWorker",
                          "args" => [])
