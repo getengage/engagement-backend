70440127b2c7af3ea787fec5953595c8ed76d1f3
Nick Soto <nickesoto@gmail.com>
2016-10-30 15:25:13 -0400

    add ApikeysController#destroy tests

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -4,7 +4,7 @@ class ApiKey < ActiveRecord::Base
   acts_as_paranoid column: :expired_at
 
   has_one :client_api_key
-  has_one :client, through: :client_api_keys
+  has_one :client, through: :client_api_key
 
   def display_name
     name.titleize
---
+++ spec/factories/client_api_keys.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :client_api_key do
+    client
+    api_key
+  end
+end
