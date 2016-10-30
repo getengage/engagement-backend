90e64aa230c92e2fc0d73cbd015f2beb3837f372
Nick Soto <nickesoto@gmail.com>
2016-10-30 03:13:46 -0400

    add ApiKeysController#create specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :user do
     confirmed_at Time.now
-    name "Test User"
-    email "test@example.com"
+    name Faker::Name.name
+    email Faker::Internet.email
     password "please1234" # min password is 10
+    client
 
     trait :admin do
       role 'admin'
     end
-
   end
 end
