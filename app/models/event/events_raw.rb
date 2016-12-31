51ae04915da4626a115a895f2f656622fb8a8d8a
Nick Soto <nickesoto@gmail.com>
2016-12-31 11:37:47 -0500

    fix scope

--- app/models/event/events_raw.rb
+++ app/models/event/events_raw.rb
@@ -4,6 +4,11 @@ module Event
       "date_part('month', NEW.created_at)"
     end
 
+    # the number of the month within the year (1 - 12)
+    scope :by_month, ->(month) {
+      where("date_part('month', created_at) = ?", month)
+    }
+
     include Base
   end
 end
