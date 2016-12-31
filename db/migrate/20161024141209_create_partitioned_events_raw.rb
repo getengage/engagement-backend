0cb8f01e01e072196658168bc2ba7b68eac8cff0
Nick Soto <nickesoto@gmail.com>
2016-12-31 15:28:59 -0500

    check timestamp

--- db/migrate/20161024141209_create_partitioned_events_raw.rb
+++ db/migrate/20161024141209_create_partitioned_events_raw.rb
@@ -1,7 +1,7 @@
 class CreatePartitionedEventsRaw < ActiveRecord::Migration
   def up
     1.upto(12) do |month|
-      execute "CREATE TABLE events_raw_#{month} (CHECK (date_part('month', created_at) = '#{month}')) INHERITS (events_raw);"
+      execute "CREATE TABLE events_raw_#{month} (CHECK (date_part('month', timestamp) = '#{month}')) INHERITS (events_raw);"
       add_index "events_raw_#{month}", :api_key_id
     end
   end
