cc781ffe33f236bb000bd51cc05947914c19113c
Nick Soto <nickesoto@gmail.com>
2017-03-25 21:35:17 -0400

    add factory for events processed

---
+++ spec/models/event/events_processed.rb
@@ -0,0 +1,15 @@
+describe Event::EventsProcessed do
+
+  describe "#top_scores_and_visits" do
+  end
+
+  describe "#aggregate_counts" do
+  end
+
+  describe "#scores_from_past_days" do
+  end
+
+  describe "#scores_from_past_week" do
+  end
+
+end
