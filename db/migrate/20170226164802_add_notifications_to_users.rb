3ec1e0d8568ee25529e50398ba5bf6e0c97f2e35
Nick Soto <nickesoto@gmail.com>
2017-03-28 17:28:33 -0400

    EventsProcessed#aggregate_counts test

--- db/migrate/20170226164802_add_notifications_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddNotificationsToUsers < ActiveRecord::Migration
-  def change
-    enable_extension "hstore"
-    add_column :users, :notifications, :hstore, default: '', null: false
-  end
-end
